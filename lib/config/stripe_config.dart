15241a87dfdabee9cf47ba13681c23a9e613775e
Wine Chit Paing <54303784+winechit-dev@users.noreply.github.com>
2025-06-12 15:05:10 +0700

    Update stripe_config.dart

--- lib/config/stripe_config.dart
+++ lib/config/stripe_config.dart
@@ -1,9 +1,9 @@
 class StripeConfig {
   // Test publishable key - Replace with your test publishable key from Stripe Dashboard
-  static const String publishableKey = 'pk_test_51RY1ejFEghzqLZCUDP9eJu59FSV0MMJZg0Cl9gZ3VrxBrTDCDzD7clWkO1MWoGddhG6UYwAqOEyje67ULUUZkh4F00Xaqmdd1F';
+  static const String publishableKey = 'your_publishableKey';
   
   // Test secret key - Replace with your test secret key from Stripe Dashboard
-  static const String secretKey = 'sk_test_51RY1ejFEghzqLZCUclSxAPGkxaGT50WIL5sK7sfTCQ9rgcX6rBJqP6DBobH1IEHccFHcRqqpGTlUphQeESTdxqQS00kBtbE9mg';
+  static const String secretKey = 'your_secretKey';
   
   // Your backend API URL for creating payment intents
   static const String apiUrl = 'https://api.stripe.com/v1/payment_intents';
